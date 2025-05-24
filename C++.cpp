fb83f41cc8877554f8ebbe59839c2ff44df90af2
Shah <irfanshahxvi@gmail.com>
2025-05-24 20:49:52 +0800

    Outputs the current RCDCS1101A GitHub Members

--- C++.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  cout << "Hai Semuaa!!!" << endl;
-  return 0;
-}
---
+++ RCDCS1101A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "\nRCDCS1101A:" << endl;
+  cout << "1. shahxvi" << endl;
+  cout << "2. Revovix" << endl;
+  cout << "3. rinnnn17" << endl;
+  cout << "4. anonymoussmk" << endl;
+  cout << "5. violetgrey8" << endl;
+  cout << "6. egyptianAiman" << endl;
+
+  return 0;
+}
